6d61c7d5fff4bed0753bb378affa9bf14f25c90e
nayeongdev <nayeongkim.dev@gmail.com>
2023-12-19 02:58:14 +0900

    :sparkles: Feat: 카카오 소셜 로그인 및 서비스 로그아웃 처리
    - 기능
     - 카카오계정과 함께 로그아웃 추가
     - 카카오계정 로그아웃 후, 핫케이크 서비스의 토큰을 블랙리스트에 추가
     - 완료되면 200 응답
    
    Resolves: #32

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 from .models import User
 
@@ -15,7 +16,7 @@ class UserSerializer(serializers.Serializer):
     class Meta:
         model = User
         fields = ["social_type", "social_id", "email", "nickname", "profile_img", "bio"]
-        read_only_fields = ('social_id', 'social_type', 'created_at', 'last_login')
+        read_only_fields = ("social_id", "social_type")
 
     def validate(self, data):
         user = authenticate(social_id=data.get("social_id"))
@@ -26,3 +27,24 @@ class UserSerializer(serializers.Serializer):
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
         return user
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh_token = serializers.CharField()
+
+    def validate(self, data):
+        """토큰 유효 검증"""
+        try:
+            self.token = data["refresh_token"]
+        except:
+            raise serializers.ValidationError({"error": "Invalid Token"})
+
+        return data
+
+    def save(self, **kwargs):
+        """토큰을 블랙리스트에 추가"""
+        try:
+            RefreshToken(self.token).blacklist()
+
+        except TokenError as ex:
+            raise exceptions.AuthenticationFailed(ex)
