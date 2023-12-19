41d14bcf046664006d5a00730495320e0b16036a
ehddn <ehddn2202@gmail.com>
2023-12-19 19:20:15 +0900

    ✨ Feature: Swagger에 필요한 설정
    - 변경사항
      - requirements.txt 에 관련 패키지를 추가하였습니다
      - settings.py에 swagger 설정 추가하였습니다
      - 프로젝트 url에 swagger에 필요한 url 추가하였습니다

--- hotcake/urls.py
+++ hotcake/urls.py
@@ -1,24 +1,16 @@
-"""
-URL configuration for hotcake project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+
+# swagger
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView, SpectacularJSONAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('accounts.urls')),
     path('trend-missions/',include('trend_missions.urls')),
+
+    # swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), # API 스키마 제공(yaml파일)
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), # 테스트할 수 있는 UI
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'), # API 문서화를 위한 UI
 ]
