3a8afb3c0d2b6e3c168ccd100bf062d6548ae156
hantang820 <hantang820@gmail.com>
2023-12-21 02:33:17 +0900

    ✨ Feature: 메인 페이지의 친구 조회하기 수정
    - 기능
      - 수정 전: 트렌드 인증 미션 참여 기록이 있는 친구들의 모든 트렌드 인증 아이템 조회
      - 수정 후: 트렌드 인증 미션 참여 기록이 있는 친구들의 특정 트렌드 인증 아이템 조회
      - 특정 트렌드 선정 기준: 인증 미션이 가장 최근 업데이트 된 트렌드

--- trends/views.py
+++ trends/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .models import Trend, TrendItem
-from accounts.models import User, Follow
-from trend_missions.models import UserTrendItem, TrendMission
+from accounts.models import Follow
+from trend_missions.models import UserTrendItem
 
-from .serializers import TrendSerializer, TrendItemSerializer, TrendViewCountSerializer
-from trend_missions.serializers import UserTrendItemSerializer
+from .serializers import (
+    TrendSerializer,
+    TrendItemSerializer,
+    TrendViewCountSerializer,
+    TrendsUserTrendItemSerializer,
+)
 from accounts.serializers import UserSerializer
 
 
@@ -15,36 +19,42 @@ class TrendView(GenericAPIView):
     """핫 트렌드 페이지 조회"""
 
     def get(self, request):
-        # 트렌드 조회
+        # 핫 트렌드 조회
         trends = Trend.objects.all()
         trend_serializer = TrendSerializer(trends, many=True)
 
         # 팔로우한 사용자들의 트렌드 아이템 조회
         user = request.user
         follow_list = Follow.objects.filter(from_user=user)
-        user_trend_item_list = []
+        user_trend_items = {}
 
+        # 가장 최근 업데이트 된 트렌드 미션의 트렌드 아이템 조회
         for follow in follow_list:
-            user_trend_items = UserTrendItem.objects.filter(user=follow.to_user)
-            user_trend_item_list.extend(user_trend_items)
+            recent_user_trend_item = (
+                UserTrendItem.objects.filter(user=follow.to_user)
+                .order_by("-updated_at")
+                .first()
+            )
+            if recent_user_trend_item is not None:
+                user_trend_item = UserTrendItem.objects.filter(
+                    trend_mission=recent_user_trend_item.trend_mission
+                )
+                user_trend_items[
+                    follow.to_user.nickname
+                ] = TrendsUserTrendItemSerializer(user_trend_item, many=True).data
 
         result = {
             "trends": trend_serializer.data,
-            "followed_trends": [],
+            "followed_trends": user_trend_items,
         }
 
         # 팔로우한 유저 여부 확인
         if not follow_list:
-            result["followed_trends"].append({"message":"아직 팔로우한 친구가 없습니다."})
+            result["followed_trends"] = {"message": "아직 팔로우한 친구가 없습니다."}
 
         # 팔로우한 유저들의 트렌드 참여 여부 확인
-        elif not user_trend_item_list:
-            result["followed_trends"].append({"message": "아직 트렌드에 참여 중인 친구가 없습니다."})
-        else:
-            trend_item_serializer = UserTrendItemSerializer(
-                user_trend_item_list, many=True
-            )
-            result["followed_trends"].extend(trend_item_serializer.data)
+        elif not user_trend_items:
+            result["followed_trends"] = {"message": "아직 트렌드에 참여 중인 친구가 없습니다."}
 
         return Response(result, status=200)
 
@@ -83,11 +93,11 @@ class TrendDetailView(GenericAPIView):
 
         # 팔로우한 유저 여부 확인
         if not follow_list:
-            result["users_with_trend"].append({"message":"아직 팔로우한 친구가 없습니다."})
+            result["users_with_trend"].append({"message": "아직 팔로우한 친구가 없습니다."})
 
         # 팔로우한 유저들의 트렌드 참여 여부 확인
         elif not users_with_trend:
-            result["users_with_trend"].append({"message":"아직 트렌드에 참여 중인 친구가 없습니다."})
+            result["users_with_trend"].append({"message": "아직 트렌드에 참여 중인 친구가 없습니다."})
         else:
             users_serializer = UserSerializer(users_with_trend, many=True)
             result["users_with_trend"] = users_serializer.data
