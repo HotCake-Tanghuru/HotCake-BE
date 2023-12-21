b1471b31b1fa22e255e8a5667495225bac27b182
hantang820 <hantang820@gmail.com>
2023-12-21 19:51:27 +0900

    ✨ Feature: 핫 트렌드 좋아요 기능
    - 기능
      - 핫 트렌드 좋아요 기능
      - 좋아요 등록과 좋아요 취소 가능
      - 이미 좋아요가 등록되어 있는 상태에서 요청한다면 좋아요 취소

--- trends/tests.py
+++ trends/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Trend
+from accounts.models import User
+
+
+class TrendLikeViewTest(TestCase):
+    """트렌드 좋아요 테스트"""
+
+    # 테스트를 위한 유저 생성
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            social_type="kakao",
+            social_id="kakao_12345",
+            email="user@email.com",
+            nickname="user",
+        )
+        self.client.force_authenticate(user=self.user)
+
+        # 테스트를 위한 트렌드 생성
+        self.trend = Trend.objects.create(
+            name="test_trend",
+        )
+
+    def test_TrendLike(self):
+        # 트렌드 좋아요 성공 테스트
+        response = self.client.patch(f"/trends/{self.trend.id}/likes/")
+        self.assertEqual(response.status_code, 200)
+
+        # 트렌드 좋아요 취소 성공 테스트
+        response = self.client.patch(f"/trends/{self.trend.id}/likes/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["message"], "좋아요 취소")
