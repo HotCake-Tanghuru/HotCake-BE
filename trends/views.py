54c57020bb72e1cc2a130ed1f5e5a13afea99fab
hantang820 <hantang820@gmail.com>
2023-12-21 22:58:12 +0900

    ♻️ Refactor: 사용되지 않은 함수 제거
    - trends/views.py에서 사용되지 않은 함수를 제거하였습니다.

--- trends/views.py
+++ trends/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
