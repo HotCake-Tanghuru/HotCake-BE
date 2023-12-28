b7f4f05bd329f47dee5e5d891357678e3d88a9d2
ehddn <ehddn2202@gmail.com>
2023-12-28 17:45:43 +0900

    ♻️ Refactor: 트렌드 미션 상세 조회 댓글 수정
    - 댓글 데이터에 comment id값을 포함하여 전달하도록 수정

--- trend_missions/serializers.py
+++ trend_missions/serializers.py
@@ -58,6 +58,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
+            "id",
             "user",
             "trend_mission",
             "content",
